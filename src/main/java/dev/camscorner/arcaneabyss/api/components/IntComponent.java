bebdd8c2876e369c653284536b950f0f5863d233
CammiePone <inazuma261@gmail.com>
2021-01-22 18:05:04 -0500

    yeeted Cardinal Components from dependencies

--- src/main/java/dev/camscorner/arcaneabyss/api/components/IntComponent.java
+++
@@ -1,9 +0,0 @@
-package dev.camscorner.arcaneabyss.api.components;
-
-import dev.onyxstudios.cca.api.v3.component.Component;
-
-public interface IntComponent extends Component
-{
-	int getValue();
-	int setValue(int value);
-}
